08b69914ab05a26a2a63d392739ec599b45cb46f
Ojwang1 <wayanicholas@gmail.com>
2022-06-24 21:59:04 +0300

    update changes

---
+++ app/src/main/java/com/example/myjudiciaryapp/Judge2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.myjudiciaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Judge2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_judge2)
+    }
+}
